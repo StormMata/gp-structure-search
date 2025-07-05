b72a89762d7a7e96cdc9a368ddf8a126c330a8bc
StormMata <storm.mata097@gmail.com>
2025-07-05 14:52:00 -0500

    working parity

--- examples/fast_example.py
+++ examples/fast_example.py
@@ -3,7 +3,7 @@
 Experiment(description='An example experiment',
            data_dir='../data/1d_data/01-airline.mat',
            results_dir='/scratch/09909/smata/induction_modeling/gaussian_process/gp_test/',
-           max_depth=3,                # How deep to run the search.
+           max_depth=5,                # How deep to run the search.
            k=1,                        # Keep the k best kernels at every iteration.  1 => greedy search.
            n_rand=2,                   # Number of random restarts.
            iters=100,                   # How long to optimize hyperparameters for.
